73a2be08ffa406fe4e5d9a23a4bd57d1bc2b1896
Jeremi Wielewski <jerwier420@gmail.com>
2021-01-12 21:16:42 +0100

    S-2

--- frontend/src/pages/Catalog/Catalog.js
+++ frontend/src/pages/Catalog/Catalog.js
@@ -1,144 +1,222 @@
 import React from "react";
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import { makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     paper: {
-        marginRight: theme.spacing(2)
-      },
+        marginRight: theme.spacing(2),
+    },
     paper2: {
         padding: theme.spacing(6),
-        textAlign: 'center',
-        height: '300px'
-      },
-  }));
+        textAlign: "center",
+        height: "300px",
+    },
+}));
 
 function Catalog() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        open: false,
+        products: [],
+    });
+    const anchorRef = React.useRef(null);
 
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+    const handleToggle = () => {
+        setState({
+            ...state,
+            open: !state.state.open,
+        });
+    };
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
 
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
+        setState({
+            ...state,
+            open: false,
+        });
+    };
+    function handleListKeyDown(event) {
+        if (event.key === "Tab") {
+            event.preventDefault();
+            setState({
+                ...state,
+                open: false,
+            });
+        }
+    }
+    function ProductCard({ name, price }) {
+        return (
+            <Grid item xs={4}>
+                <Paper className={classes.paper2}>{`${name} ${price}`}</Paper>
+            </Grid>
+        );
     }
 
-    setOpen(false);
-  };
+    // return focus to the button when we transitioned from !state.open -> state.open
+    const prevOpen = React.useRef(state.open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && state.open === false) {
+            anchorRef.current.focus();
+        }
 
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
-
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <Paper className={classes.paper2}>Product</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper2}>Product</Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper2}>Product</Paper>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+        prevOpen.current = state.open;
+    }, [state.open]);
+    React.useEffect(async () => {
+        const response = await fetch("http://localhost:4000/products");
+        const json = await response.json();
+        setState({
+            ...state,
+            products: json.Products,
+        });
+    }, []);
 
     return (
         <React.Fragment>
-            <div style={{  
-                backgroundImage: "url(" + "https://i.imgur.com/6XhoraV.png" + ")",
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'
-            }}>
+            <div
+                style={{
+                    backgroundImage:
+                        "url(" + "https://i.imgur.com/6XhoraV.png" + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                }}
+            >
                 <br></br>
                 <div className={classes.root}>
-                    <AppBar position="static" style={{backgroundColor: 'white', height: '40px'}}>
+                    <AppBar
+                        position="static"
+                        style={{ backgroundColor: "white", height: "40px" }}
+                    >
                         <Toolbar variant="dense">
-                            <Typography style={{color: 'black', fontSize: '15px', fontFamily: 'Cinzel', fontWeight: 800}}>
+                            <Typography
+                                style={{
+                                    color: "black",
+                                    fontSize: "15px",
+                                    fontFamily: "Cinzel",
+                                    fontWeight: 800,
+                                }}
+                            >
                                 Filter By
-                                <ExpandMoreIcon 
-                                ref={anchorRef}
-                                aria-controls={open ? 'menu-list-grow' : undefined}
-                                aria-haspopup="true"
-                                onClick={handleToggle}
+                                <ExpandMoreIcon
+                                    ref={anchorRef}
+                                    aria-controls={
+                                        state.open
+                                            ? "menu-list-grow"
+                                            : undefined
+                                    }
+                                    aria-haspopup="true"
+                                    onClick={handleToggle}
                                 ></ExpandMoreIcon>
                             </Typography>
-                            <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                            <Popper
+                                open={state.open}
+                                anchorEl={anchorRef.current}
+                                role={undefined}
+                                transition
+                                disablePortal
+                            >
                                 {({ TransitionProps, placement }) => (
-                                <Grow
-                                {...TransitionProps}
-                                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-                                >
-                                    <Paper>
-                                        <ClickAwayListener onClickAway={handleClose}>
-                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                                                <MenuItem onClick={handleClose}>option</MenuItem>
-                                                <MenuItem onClick={handleClose}>option</MenuItem>
-                                                <MenuItem onClick={handleClose}>option</MenuItem>
-                                            </MenuList>
-                                        </ClickAwayListener>
-                                    </Paper>
-                                </Grow>
+                                    <Grow
+                                        {...TransitionProps}
+                                        style={{
+                                            transformOrigin:
+                                                placement === "bottom"
+                                                    ? "center top"
+                                                    : "center bottom",
+                                        }}
+                                    >
+                                        <Paper>
+                                            <ClickAwayListener
+                                                onClickAway={handleClose}
+                                            >
+                                                <MenuList
+                                                    autoFocusItem={state.open}
+                                                    id="menu-list-grow"
+                                                    onKeyDown={
+                                                        handleListKeyDown
+                                                    }
+                                                >
+                                                    <MenuItem
+                                                        onClick={handleClose}
+                                                    >
+                                                        option
+                                                    </MenuItem>
+                                                    <MenuItem
+                                                        onClick={handleClose}
+                                                    >
+                                                        option
+                                                    </MenuItem>
+                                                    <MenuItem
+                                                        onClick={handleClose}
+                                                    >
+                                                        option
+                                                    </MenuItem>
+                                                </MenuList>
+                                            </ClickAwayListener>
+                                        </Paper>
+                                    </Grow>
                                 )}
                             </Popper>
-                            <Typography style={{color: 'black', fontSize: '15px', fontFamily: 'Cinzel', fontWeight: 800, paddingLeft: '40px'}}>
+                            <Typography
+                                style={{
+                                    color: "black",
+                                    fontSize: "15px",
+                                    fontFamily: "Cinzel",
+                                    fontWeight: 800,
+                                    paddingLeft: "40px",
+                                }}
+                            >
                                 Sort By
                                 <ExpandMoreIcon></ExpandMoreIcon>
                             </Typography>
-                            <Typography style={{color: 'black', fontSize: '15px', fontFamily: 'Cinzel', fontWeight: 800, paddingLeft: '40px'}}>
+                            <Typography
+                                style={{
+                                    color: "black",
+                                    fontSize: "15px",
+                                    fontFamily: "Cinzel",
+                                    fontWeight: 800,
+                                    paddingLeft: "40px",
+                                }}
+                            >
                                 <SearchIcon></SearchIcon>
                             </Typography>
                         </Toolbar>
                     </AppBar>
                 </div>
                 <br></br>
-                <div style={{margin: 'auto', width: '90%'}}>
-                <Grid container spacing={9}  style={{paddingTop:'30px', width: '90%', margin: 'auto'}}>
-                    <Grid container item xs={12} spacing={9}>
-                        <FormRow />
+                <div style={{ margin: "auto", width: "90%" }}>
+                    <Grid
+                        container
+                        spacing={9}
+                        style={{
+                            paddingTop: "30px",
+                            width: "90%",
+                            margin: "auto",
+                        }}
+                    >
+                        {state.products.map((product) => (
+                            <ProductCard
+                                name={product.name}
+                                price={product.product_cost}
+                            />
+                        ))}
                     </Grid>
-                    <Grid container item xs={12} spacing={9}>
-                        <FormRow />
-                    </Grid>
-                    <Grid container item xs={12} spacing={9}>
-                        <FormRow />
-                    </Grid>
-                </Grid>
                 </div>
             </div>
         </React.Fragment>
