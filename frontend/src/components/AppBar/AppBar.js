e4573ac357e0915a973aa16cadf7986feecdb85d
Jagoda Lewandowska <jadlewandowska@gmail.com>
2021-01-31 10:51:20 +0100

    dodano element drawer

--- frontend/src/components/AppBar/AppBar.js
+++ frontend/src/components/AppBar/AppBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -6,51 +7,183 @@ import Grid from '@material-ui/core/Grid';
 import { Link } from "react-router-dom";
 import './AppBar.css'
 import Cart from '../Cart/Cart';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: '#d2c1c9'
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
 
 export default function DenseAppBar() {
   
-    const authenticated = false;
+  const authenticated = false;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
 
   return (
     <div styles={{flexGrow: 1}}>
-      <AppBar position="static" style={{height: "35px", alignItems: 'center', backgroundColor: '#d2c1c9'}}>
-        <Toolbar variant="dense">
-          <Typography style={{fontSize: '15px', fontFamily: 'Cinzel', fontWeight: 800}}>
+      <AppBar        
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography style={{fontSize: '15px', fontFamily: 'Cinzel', fontWeight: 800, alignItems: 'center'}}>
             FREE SHIPPING | FREE RETURNS
           </Typography>
         </Toolbar>
       </AppBar>
-
-     {!authenticated && (
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['All products', 'Sign In', 'Sign Out', 'Cart'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Sign In', 'Sign Out', 'All products'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+        {!authenticated && (
+              <Grid
+              container
+              direction="row-reverse"
+              justify="flex-start"
+              alignItems="baseline"
+              style={{fontSize: '20px', fontFamily: 'Cinzel', fontWeight: 800, paddingTop: '10px'}}
+              >
+                  <Link style={{paddingRight: '15px', textDecoration: 'none', color: 'black'}} to="/signup">Sign Up</Link>
+                  <Link style={{paddingRight: '10px', textDecoration: 'none', color: 'black'}} to="/signin">Sign In</Link>
+              </Grid>
+        )}
           <Grid
           container
-          direction="row-reverse"
-          justify="flex-start"
-          alignItems="baseline"
-          style={{fontSize: '20px', fontFamily: 'Cinzel', fontWeight: 800, paddingTop: '10px'}}
+          direction="column"
+          justify="space-between"
+          alignItems="center"
+          style={{paddingTop:'40px'}}
           >
-              <Link style={{paddingRight: '15px', textDecoration: 'none', color: 'black'}} to="/signup">Sign Up</Link>
-              <Link style={{paddingRight: '10px', textDecoration: 'none', color: 'black'}} to="/signin">Sign In</Link>
+          <img src='https://i.imgur.com/cVrvjAS.png' alt="logo" style={{width: '150px'}}></img>
           </Grid>
-     )}
-      <Grid
-      container
-      direction="column"
-      justify="space-between"
-      alignItems="center"
-      style={{paddingTop:'15px'}}
-      >
-      <img src='https://i.imgur.com/cVrvjAS.png' alt="logo" style={{width: '150px'}}></img>
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justify="space-between"
-        alignItems="center"
-        style={{fontSize:'20px', fontFamily: 'Cinzel', fontWeight: 800, paddingBottom: '10px'}}
-      >
-      
-      </Grid>  
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            alignItems="center"
+            style={{fontSize:'20px', fontFamily: 'Cinzel', fontWeight: 800, paddingBottom: '10px'}}
+          >
+          
+          </Grid>
       </div>
   );
 }
