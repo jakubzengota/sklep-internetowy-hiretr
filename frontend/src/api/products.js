175af7ab4753c85e0a69cfc27f070ea4f0505c39
Jakub Zengota <jakubzengota@gmail.com>
2021-03-22 09:38:02 +0100

    HIR-17

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -16,7 +16,7 @@ export const getProducts = memoize(async ({ offset, limit }) => {
     const json = await response.json();
     return json.products;
 });
-
+//
 export const getProductById = memoize(async (productId) => {
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     const json = await response.json();
