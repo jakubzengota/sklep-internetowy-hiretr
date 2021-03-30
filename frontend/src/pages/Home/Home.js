cf85c37e9c76cae8e06dd153d646de95ec34b965
Jakub Zengota <jakubzengota@gmail.com>
2021-03-30 17:51:36 +0200

    carousel

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import CarouselSlide from './CarouselSlide';
+import './Home.css';
+import { SLIDE_INFO } from './constants';
 
 function Home() {
+    const content = SLIDE_INFO[3];
     return (
         <React.Fragment>
+            <div className='Home'>
+            <CarouselSlide
+                content={content}
+            />
+        </div>
             <img src='https://i.imgur.com/I5r4pqY.png' alt='homepagepic' style={{width: '100%'}}></img>
         </React.Fragment>
     );
